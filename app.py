8ea0a41393ebbde4a06969b6ed4a976e422b7101
Rishabh <rishabhbhardwaj1234567@gmail.com>
2021-05-09 06:41:46 +0530

    deploy 6

--- app.py
+++ app.py
@@ -68,5 +68,5 @@ def getTag():
             return jsonify(profile_json)
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=os.environ.get('PORT', '5000'), debug=True)
